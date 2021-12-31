6a9afcd370e1641a30cc463c852e26acced6f865
Todd Erickson <todd1981@gmail.com>
2021-12-30 19:42:51 -0600

    updating deployment file to work for both master bedroom and family room

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 scp -r . pi@192.168.72.20:/usr/apps/tv-api/
+scp -r . pi@192.168.72.25:/usr/apps/tv-api/
 ssh pi@192.168.72.20 "sudo systemctl restart tv-api.service"
+ssh pi@192.168.72.25 "sudo systemctl restart tv-api.service"
 sleep 2
 ssh pi@192.168.72.20 "sudo systemctl status tv-api.service"
+ssh pi@192.168.72.25 "sudo systemctl status tv-api.service"
+
